39334b988c06c105473d87b85e367168fd7a2fe4
prag06 git <pragyagpt21@gmail.com>
2026-08-06 17:06:35 +0530

    updated backend API url

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://inotebook-4gy6.onrender.com";
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   let Navigate = useNavigate();
   const handleSubmit = async (e) => {
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signup = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://inotebook-4gy6.onrender.com";
   const [credentials, setCredentials] = useState({
     name: "",
     email: "",
