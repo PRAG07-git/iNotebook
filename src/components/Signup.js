4d75d27bd6d0edc23bce0214bab0ead207a46f7a
prag06 git <pragyagpt21@gmail.com>
2026-08-08 15:38:08 +0530

    fixed eslint unused json warnings

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signup = (props) => {
-  const host = "http://localhost:5000";
+  const host = process.env.REACT_APP_HOST || "http://localhost:5000";
   const [credentials, setCredentials] = useState({
     name: "",
     email: "",
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -39,7 +39,7 @@ const NoteState = (props) => {
         "auth-token": localStorage.getItem("token"),
       },
     });
-    const json = response.json;
+    await response.json;
 
     const newNotes = notes.filter((note) => {
       return note._id !== id;
@@ -55,7 +55,7 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag }),
     });
-    const json = await response.json();
+    await response.json();
     let newNotes = JSON.parse(JSON.stringify(notes));
     for (let index = 0; index < notes.length; index++) {
       const element = notes[index];
